a97fdb95cf25979ce677be0db0ee9540d02841f0
Lee Baillie <lw.baillie@gmail.com>
2021-09-09 13:43:21 +0100

    remove connection

--- server.js
+++ server.js
@@ -11,44 +11,6 @@ const mongoose = require('mongoose');
 
 const port = process.env.PORT || 3000
 
-// Connection Details
-// const connection = mysql.createConnection({
-//   host: 'eu-cdbr-west-01.cleardb.com',
-//   user: 'b864459d35e3fd',
-//   password: '6a880cda',
-//   database: 'heroku_4eb7a8ff5383ffb'
-// })
-
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://dbAdmin:5EBi5shIIOry6pfa@cluster0.ca8lk.mongodb.net/blog?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log('Database connected sucessfully !')
-},
-    error => {
-        console.log('Database could not be connected : ' + error)
-    }
-)
-
-
-// const connectDB = async () => {
-//     try {
-//       await mongoose.connect('mongodb+srv://dbAdmin:5EBi5shIIOry6pfa@cluster0.ca8lk.mongodb.net/blog?retryWrites=true&w=majority', {
-//         useNewUrlParser: true,
-//         useFindAndModify: true,
-//         useUnifiedTopology: true,
-//         useCreateIndex: true,
-//       });
-  
-//       console.log("MongoDB Connection Success 👍");
-//     } catch (error) {
-//       console.log("MongoDB Connection Failed 💥");
-//       process.exit(1);
-//     }
-//   };
-//   connectDB()
-
 // View engine
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/public'));
